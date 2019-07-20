9c39c5b590dc5c89e87094a695030a56e4af03a9
piruty <8793641+piruty@users.noreply.github.com>
2019-07-20 19:04:39 +0900

    imageモデルを追加（使用予定でないviewも一応作成）

---
+++ db/migrate/20190720095926_create_images.rb
@@ -0,0 +1,11 @@
+class CreateImages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :images do |t|
+      t.string :filename
+      t.string :path
+      t.string :hash_val
+
+      t.timestamps
+    end
+  end
+end
